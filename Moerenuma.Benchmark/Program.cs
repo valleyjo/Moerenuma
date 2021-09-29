ea3ae7b7314201a919b60b87b7e24855351d727f
Alex Vallejo <vallejo.alex@gmail.com>
2021-09-29 12:02:28 -0700

    Add benchmarks:

---
+++ Moerenuma.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Moerenuma.Benchmark
+{
+  using BenchmarkDotNet.Reports;
+  using BenchmarkDotNet.Running;
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Summary summary = BenchmarkRunner.Run<ToLowerBenchmark>();
+    }
+  }
+}
---
+++ Moerenuma.Benchmark/ToLowerBenchmark.cs
@@ -0,0 +1,32 @@
+﻿namespace Moerenuma.Benchmark
+{
+  using System;
+  using System.Text;
+  using BenchmarkDotNet.Attributes;
+
+  [MemoryDiagnoser]
+  public class ToLowerBenchmark
+  {
+    private readonly string data;
+
+    public ToLowerBenchmark()
+    {
+      var sb = new StringBuilder();
+      var random = new Random();
+      int strlen = 10_000;
+      for (int i = 0; i < strlen; i++)
+      {
+        char c = (char)random.Next('A', 'z');
+        sb.Append(c);
+      }
+
+      this.data = sb.ToString();
+    }
+
+    [Benchmark]
+    public string ToLower() => this.data.ToLower();
+
+    [Benchmark]
+    public string ToLowerInvariant() => this.data.ToLowerInvariant();
+  }
+}
