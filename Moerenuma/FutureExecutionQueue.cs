90d2350bf79d57b8cfcb06a39a8ced8dc7d3930d
Alex Vallejo <vallejo.alex@gmail.com>
2021-10-15 23:04:49 -0700

    Add future execution queue

---
+++ Moerenuma/FutureExecutionQueue.cs
@@ -0,0 +1,106 @@
+﻿namespace Moerenuma
+{
+  using System;
+  using System.Collections.Concurrent;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  public class FutureExecutionQueue : IDisposable
+  {
+    private readonly SemaphoreSlim semephore;
+    private readonly Timer timer;
+    private readonly CancellationToken token;
+    private ConcurrentQueue<FutureExecutionRecord> queue;
+    private DateTime nextExecutionTime;
+
+    public FutureExecutionQueue(CancellationToken token)
+    {
+      this.token = token;
+      this.nextExecutionTime = DateTime.MaxValue;
+      this.queue = new ConcurrentQueue<FutureExecutionRecord>();
+      this.semephore = new SemaphoreSlim(0, 1);
+      this.timer = new Timer((o) => this.TimerCallback());
+    }
+
+    public void Enqueue(int delayMs, Action act)
+    {
+      this.token.ThrowIfCancellationRequested();
+      this.EnqueueRecord(new FutureExecutionRecord(delayMs, act));
+    }
+
+    public async Task ExecuteAsync()
+    {
+      while (true)
+      {
+        await this.semephore.WaitAsync(this.token);
+        ConcurrentQueue<FutureExecutionRecord> workToDo = Interlocked.Exchange(ref this.queue, new ConcurrentQueue<FutureExecutionRecord>());
+        while (workToDo.Count > 0)
+        {
+          this.token.ThrowIfCancellationRequested();
+          if (workToDo.TryDequeue(out FutureExecutionRecord record))
+          {
+            if (record.ShouldExecute)
+            {
+              record.Execute();
+            }
+            else
+            {
+              this.EnqueueRecord(record);
+            }
+          }
+        }
+      }
+    }
+
+    public void Dispose()
+    {
+      this.timer.Dispose();
+      this.semephore.Dispose();
+    }
+
+    private void EnqueueRecord(FutureExecutionRecord record)
+    {
+      this.queue.Enqueue(record);
+      if (record.DueTime < this.nextExecutionTime)
+      {
+        this.timer.Change(record.DelayTime, Timeout.InfiniteTimeSpan);
+        this.nextExecutionTime = record.DueTime;
+      }
+    }
+
+    private void TimerCallback()
+    {
+      this.nextExecutionTime = DateTime.MaxValue;
+      if (this.semephore.CurrentCount == 0)
+      {
+        this.semephore.Release();
+      }
+    }
+
+    private record FutureExecutionRecord
+    {
+      public FutureExecutionRecord(int delayMs, Action action)
+      {
+        this.Action = action;
+        this.DueTime = DateTime.UtcNow + TimeSpan.FromMilliseconds(delayMs);
+      }
+
+      public Action Action { get; private set; }
+
+      public TimeSpan DelayTime
+      {
+        get
+        {
+          DateTime now = DateTime.UtcNow;
+          return now > this.DueTime ? TimeSpan.Zero : this.DueTime - now;
+        }
+      }
+
+      public DateTime DueTime { get; private set; }
+
+      public bool ShouldExecute => DateTime.UtcNow >= this.DueTime;
+
+      public void Execute() => this.Action();
+    }
+  }
+}
--- Moerenuma/Program.cs
+++ Moerenuma/Program.cs
@@ -1,12 +1,59 @@
 ﻿namespace Moerenuma
 {
-  using Moerenuma.Channels;
+  using System;
+  using System.Threading;
+  using System.Threading.Tasks;
 
   public class Program
   {
+    private const string DateTimeFormat = "hh:mm:ss.ffff";
+
     public static void Main(string[] args)
     {
-      ChannelRunner.Run();
+      var cts = new CancellationTokenSource();
+      var feq = new FutureExecutionQueue(cts.Token);
+      Task queueTask = feq.ExecuteAsync();
+
+      Console.WriteLine("Enter -1 to exit");
+      Console.WriteLine("Enter -2 to cancel the TokenSource");
+      while (true)
+      {
+        LogWithTime("Enter the task callback delay in ms: ");
+        string input = Console.ReadLine();
+        int delayMs = int.Parse(input);
+        DateTime expectedDueTime = DateTime.Now + TimeSpan.FromMilliseconds(delayMs);
+        LogWithTime($"Entered '{delayMs}'. Expected due back at {expectedDueTime.ToString(DateTimeFormat)}");
+
+        if (delayMs == -1)
+        {
+          break;
+        }
+        else if (delayMs == 5)
+        {
+          // special value to indicate a delay on the execution
+          // can simulate executing a lot of actions
+          string msg = $"callback executed for delay '{delayMs}'";
+          feq.Enqueue(delayMs, () =>
+          {
+            Thread.Sleep(5000);
+            LogWithTime(msg);
+          });
+        }
+        else if (delayMs == -2)
+        {
+          cts.Cancel();
+          Console.WriteLine("Cancelling the token");
+        }
+        else
+        {
+          string msg = $"callback executed for delay '{delayMs}'";
+          feq.Enqueue(delayMs, () => LogWithTime(msg));
+        }
+      }
+
+      Console.WriteLine("Exiting...");
     }
+
+    private static void LogWithTime(string msg) => Console.WriteLine($"[{DateTime.Now.ToString(DateTimeFormat)}]: {msg}");
   }
 }
